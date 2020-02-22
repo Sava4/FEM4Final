8dd2b700e07db7c3eb6488235ed81e068ee05a4f
Anton Molchanovskyi <mywear1@gmail.com>
2020-02-23 00:25:57 +0200

    url_almost2

---
+++ client/src/Components/index.js
@@ -0,0 +1,4 @@
+export { Header } from "./Header/header";
+export { Spinner } from "./Spinner/spinner";
+export { Footer } from "./Footer/Footer.js";
+export { SliderHomepage } from "./Slider/Slider.js";
---
+++ client/src/Views/staticPage.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+// import { Layout } from "./layout";
+import { Header, Footer } from "../Components";
+import {
+  mediaDesktop,
+  mediaMobile,
+  mediaTablet
+} from "../styled-components/media-breakpoints-mixin";
+
+import { Url2 } from "../../src/App";
+
+export const StaticPage = props => {
+  console.log(props);
+  const url = props.location.pathname;
+  console.log(url);
+
+  const [page, setPage] = useState({});
+  useEffect(() => {
+    // debugger;
+    fetch(`http://localhost:5000/pages/${url}`)
+      .then(res => {
+        return res.json();
+      })
+      .then(json => {
+        setPage(json);
+      });
+  }, [url]);
+
+  return (
+    <div>
+      <Header />
+      <Page>
+        {/* <StaticPage /> */}
+        {/* {console.log(url)} */}
+        <p>{page.htmlContent}</p>
+        {/* <Url2 url={`${url}`}/> */}
+      </Page>
+      <Footer />
+    </div>
+  );
+};
+
+const Page = styled.div` 
+  margin: 0 auto;
+    
+    ${mediaDesktop(`
+      width: 80%;     
+    `)}
+    ${mediaTablet(`
+    width: 80%;     
+    `)}
+    ${mediaMobile(`
+    width: 80%;    
+    `)}
+`;
