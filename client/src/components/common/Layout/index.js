2d8706a6865e56ff053638f3dfec3a99126cdf1a
Alexander Savchenko <sasha.savchenko@gmail.com>
2020-02-25 14:53:53 +0200

    Try to pass tests

--- client/src/components/common/Layout/index.js
+++ client/src/components/common/Layout/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 
-import { Header } from "../Header";
+import { Header } from "../Header/";
 import { Footer } from "../Footer";
 
 export const Layout = props => {
