13e4170084382c18917f90cbc82126829ccb453c
Kseniia Zinchenko <ukrksenia2@gmail.com>
2020-02-26 00:10:52 +0200

    Removed comments

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,18 @@
 import React from "react";
-// import { Provider } from "react-redux";
-// import { Routes } from "./routes";
-// import { BrowserRouter } from "react-router-dom";
-import { ListProduct } from "./components/ProductsList/ProductList"
-// import store from "./store";
+import { Provider } from "react-redux";
+import { Routes } from "./routes";
+import { BrowserRouter } from "react-router-dom";
+import store from "./store";
 
-// import { Header, Spinner } from "./components";
+import { Header, Spinner } from "./components";
 
 function App() {
   return (
-    //<Provider store={store}>
-    //  <BrowserRouter>
-    //    <Routes />
-    //  </BrowserRouter>
-    //</Provider>
-      <ListProduct/>
+    <Provider store={store}>
+     <BrowserRouter>
+       <Routes />
+     </BrowserRouter>
+    </Provider>
   );
 }
 
