1dd7980760fec4c0bd1b353d50d4abf22a32e3c4
Kseniia Zinchenko <ukrksenia2@gmail.com>
2020-02-26 21:56:46 +0200

    Alignment applied

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { Provider } from "react-redux";
-import { Routes } from "./routes";
-import { BrowserRouter } from "react-router-dom";
+import {Provider} from "react-redux";
+import {Routes} from "./routes";
+import {BrowserRouter} from "react-router-dom";
 import store from "./store";
 
-// import { Header, Spinner } from "./components";
+//import {Header, Spinner} from "./components";
 
 function App() {
     return (
