98c47c378d93b7a87f0dbb2bcb3f3ae58a3053f1
Anton Molchanovskyi <mywear1@gmail.com>
2020-02-28 21:09:42 +0200

    ready2go

--- client/src/components/common/Footer/newSubscriber.js
+++ client/src/components/common/Footer/newSubscriber.js
@@ -10,31 +10,31 @@ export function AddSubscriber(props) {
       "<!DOCTYPE html><html lang='en'> <head> <meta charset='UTF-8' /> <meta name='viewport' content='width=device-width, initial-scale=1.0' /> <meta http-equiv='X-UA-Compatible' content='ie=edge' /> <title>Document</title> <style> td { padding: 20px 50px; background-color: yellow; color: blueviolet; font-size: 20px; } </style> </head> <body> <table> <tr> <td>Test1</td> <td>Test2</td> <td>Test3</td> </tr> <tr> <td>Test1.1</td> <td>Test2.1</td> <td>Test3.1</td> </tr> </table> </body></html>"
   };
   // console.log(email);
-
-  axios
-    .post("http://localhost:5000/customers/login", {
-      loginOrEmail: "customer@gmail.com",
-      password: "1111111"
-    })
-    .then(response => {
-      let token = response.data.token;
-      axios
-        .post("http://localhost:5000/subscribers", newSubscriber, {
-          headers: { Authorization: `${token}` }
-        })
-        .then(newSubscriber => {
-          // console.log("success");
-          // console.log(newSubscriber);
-        })
-        .catch(err => {
-          // console.log("error add");
-          // console.log(err);
-        });
-    })
-    .catch(err => {
-      console.log("error auth");
-      console.log(err);
-    });
-
+  if (email !== "") {
+    axios
+      .post("http://localhost:5000/customers/login", {
+        loginOrEmail: "customer@gmail.com",
+        password: "1111111"
+      })
+      .then(response => {
+        let token = response.data.token;
+        axios
+          .post("http://localhost:5000/subscribers", newSubscriber, {
+            headers: { Authorization: `${token}` }
+          })
+          .then(newSubscriber => {
+            // console.log("success");
+            // console.log(newSubscriber);
+          })
+          .catch(err => {
+            // console.log("error add");
+            // console.log(err);
+          });
+      })
+      .catch(err => {
+        console.log("error auth");
+        console.log(err);
+      });
+  }
   return <div></div>;
 }
