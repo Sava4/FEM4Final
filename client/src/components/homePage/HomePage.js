b218adef3090b531f7cfccebbc3785995c2f9e92
Olya Markysh <december.olka51@gmail.com>
2020-03-04 15:10:04 +0200

    fixed section explore categories, add mediaqueries

--- client/src/components/homePage/HomePage.js
+++ client/src/components/homePage/HomePage.js
@@ -1,15 +1,14 @@
 import React from "react";
-// import styled from "styled-components"
-import { SliderHomepage } from "../Slider/Slider";
 
 import { Layout } from "../common/Layout";
-import { HomepageCategiries } from "./categories";
+import { SliderHomepage } from "../Slider/Slider";
+import { HomepageCategories } from "./categories";
 
 export const Homepage = () => {
   return (
     <Layout>
       <SliderHomepage />
-      <HomepageCategiries></HomepageCategiries>
+      <HomepageCategories></HomepageCategories>
     </Layout>
   );
 };
