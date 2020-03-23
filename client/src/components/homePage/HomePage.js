90988688203a7c6d2e1c42b12f789aa966ec16f3
Kseniia Zinchenko <ukrksenia2@gmail.com>
2020-03-23 11:30:44 +0200

    Deleted ProductList

--- client/src/components/homePage/HomePage.js
+++ client/src/components/homePage/HomePage.js
@@ -3,9 +3,7 @@ import React from "react";
 import { SliderHomepage } from "../Slider/Slider";
 import { SliderProducts } from "../SliderProducts/SliderProducts";
 import { Layout } from "../common/Layout";
-import { ProductsList } from "../ProductsList/productList";
 import { HomepageCategories } from "./categories";
-import { ProductDetails } from "./../ProductDetails/productDetails";
 import { SelectedCollections } from "./selectedCollections/selectedCollections";
 import { AboutCompany } from "./aboutCompany/aboutCompany";
 
@@ -14,7 +12,6 @@ export const Homepage = () => {
     <Layout>
       <SliderHomepage />
       <SliderProducts h4={"FEATURED"} reverse={"reverse"} />
-      <ProductsList />
       <HomepageCategories />
       <SelectedCollections />
       <AboutCompany />
