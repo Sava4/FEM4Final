b1323c64041be66d784de6f4c4d06509a9450dc9
Kseniia Zinchenko <ukrksenia2@gmail.com>
2020-03-28 05:48:54 +0200

    Added maps and addresses of stores

---
+++ client/src/components/Map/citiesContactsList.js
@@ -0,0 +1,563 @@
+import React from "react";
+import shop from "./shop.svg";
+import phone from "./phone.svg";
+import clock from "./clock.svg";
+import styled, { css } from "styled-components";
+
+export const Kyiv = () => {
+  return (
+    <Container>
+      <ContactWrapper>
+        <Text weight={"bold"}>Zarina Jewelry House</Text>
+        <Text>
+          <Image alt="" src={shop} /> st. Hnata Khotkevycha, 1-B
+        </Text>
+        <Text>
+          <Image alt="" src={phone} /> tel. +38 (063) 030 27 38
+        </Text>
+        <Text>
+          <Image alt="" src={clock} /> Mon-Sun: 10am - 10pm
+        </Text>
+      </ContactWrapper>
+      <ContactWrapper>
+        <Text weight={"bold"}>Zarina Jewelry House</Text>
+        <Text>
+          <Image alt="" src={shop} /> st. Zdolbunivska, 17
+        </Text>
+        <Text>
+          <Image alt="" src={phone} /> tel. +38 (068) 525 11 22
+        </Text>
+        <Text>
+          <Image alt="" src={clock} /> Mon-Sun: 10am - 9pm
+        </Text>
+      </ContactWrapper>
+      <ContactWrapper>
+        <Text weight={"bold"}>Zarina Jewelry House</Text>
+        <Text>
+          <Image alt="" src={shop} /> ave. Stepana Bandery, 23
+        </Text>
+        <Text>
+          <Image alt="" src={phone} /> tel. +38 (063) 374 66 92
+        </Text>
+        <Text>
+          <Image alt="" src={clock} /> Mon-Sun: 10am - 9pm
+        </Text>
+      </ContactWrapper>
+      <ContactWrapper>
+        <Text weight={"bold"}>Zarina Jewelry House</Text>
+        <Text>
+          <Image alt="" src={shop} /> ave. Tarasa Shevchenka, 2
+        </Text>
+        <Text>
+          <Image alt="" src={phone} /> tel. +38 (044) 235 62 22
+        </Text>
+        <Text>
+          <Image alt="" src={clock} /> Mon-Sut: 10am - 8pm
+        </Text>
+      </ContactWrapper>
+      <ContactWrapper>
+        <Text weight={"bold"}>Zarina Jewelry House</Text>
+        <Text>
+          <Image alt="" src={shop} /> st. Gryshka, 3a
+        </Text>
+        <Text>
+          <Image alt="" src={phone} /> tel. +38 (044) 577 36 70
+        </Text>
+        <Text>
+          <Image alt="" src={clock} /> Mon-Sun: 10am - 10pm
+        </Text>
+      </ContactWrapper>
+      <ContactWrapper>
+        <Text weight={"bold"}>Zarina Jewelry House</Text>
+        <Text>
+          <Image alt="" src={shop} /> sq. Bessarabska
+        </Text>
+        <Text>
+          <Image alt="" src={phone} /> tel. +38 (044) 247 55 43
+        </Text>
+        <Text>
+          <Image alt="" src={clock} /> Mon-Sun: 10am - 8:30pm
+        </Text>
+      </ContactWrapper>
+      <ContactWrapper>
+        <Text weight={"bold"}>Zarina Jewelry House</Text>
+        <Text>
+          <Image alt="" src={shop} /> st. Dniprovska Naberezhna, 12
+        </Text>
+        <Text>
+          <Image alt="" src={phone} /> tel. +38 (096) 802 06 00
+        </Text>
+        <Text>
+          <Image alt="" src={clock} /> Mon-Sun: 10am - 10pm
+        </Text>
+      </ContactWrapper>
+      <ContactWrapper>
+        <Text weight={"bold"}>Zarina Jewelry House</Text>
+        <Text>
+          <Image alt="" src={shop} /> Mall '4Room'
+        </Text>
+        <Text>
+          <Image alt="" src={phone} /> tel.+38 (063) 869 90 96
+        </Text>
+        <Text>
+          <Image alt="" src={clock} /> Mon-Sun: 10am - 8pm
+        </Text>
+      </ContactWrapper>
+      <ContactWrapper>
+        <Text weight={"bold"}>Zarina Jewelry House</Text>
+        <Text>
+          <Image alt="" src={shop} /> st. Antonovycha, 176
+        </Text>
+        <Text>
+          <Image alt="" src={phone} /> tel. +38 (096) 900 79 10
+        </Text>
+        <Text>
+          <Image alt="" src={clock} /> Mon-Sun: 10am - 10pm
+        </Text>
+      </ContactWrapper>
+      <ContactWrapper>
+        <Text weight={"bold"}>Zarina Jewelry House</Text>
+        <Text>
+          <Image alt="" src={shop} /> Mall 'Globus'
+        </Text>
+        <Text>
+          <Image alt="" src={phone} /> tel. +38 (073) 312 13 21
+        </Text>
+        <Text>
+          <Image alt="" src={clock} /> Mon-Sun: 10am - 10pm
+        </Text>
+      </ContactWrapper>
+      <ContactWrapper>
+        <Text weight={"bold"}>Zarina Jewelry House</Text>
+        <Text>
+          <Image alt="" src={shop} /> st. Zabolotnoho, 37
+        </Text>
+        <Text>
+          <Image alt="" src={phone} /> tel. +38 (093) 400 10 25
+        </Text>
+        <Text>
+          <Image alt="" src={clock} /> Mon-Sun: 10am - 9pm
+        </Text>
+      </ContactWrapper>
+      <ContactWrapper>
+        <Text weight={"bold"}>Zarina Jewelry House</Text>
+        <Text>
+          <Image alt="" src={shop} /> sq. Sportyvna, 1
+        </Text>
+        <Text>
+          <Image alt="" src={phone} /> tel. +38 (063) 531 34 18
+        </Text>
+        <Text>
+          <Image alt="" src={clock} /> Mon-Sun: 10am - 10pm
+        </Text>
+      </ContactWrapper>
+    </Container>
+  );
+};
+
+export const BilaTserkva = () => {
+  return (
+    <Container>
+      <ContactWrapper>
+        <Text weight={"bold"}>Zarina Jewelry House</Text>
+        <Text>
+          <Image alt="" src={shop} /> st. Yaroslava Mydrogo, 40
+        </Text>
+        <Text>
+          <Image alt="" src={phone} /> tel. +38 (063) 979 92 36
+        </Text>
+        <Text>
+          <Image alt="" src={clock} /> Mon-Sun: 9am - 7:30pm
+        </Text>
+      </ContactWrapper>
+    </Container>
+  );
+};
+
+export const Berdychiv = () => {
+  return (
+    <Container>
+      <ContactWrapper>
+        <Text weight={"bold"}>Zarina Jewelry House</Text>
+        <Text>
+          <Image alt="" src={shop} /> st. Zhytomyrska, 20
+        </Text>
+        <Text>
+          <Image alt="" src={phone} /> tel. +38 (041) 432 15 79
+        </Text>
+        <Text>
+          <Image alt="" src={clock} /> Mon-Fr: 9am - 7pm
+        </Text>
+      </ContactWrapper>
+    </Container>
+  );
+};
+
+export const Dnipro = () => {
+  return (
+    <Container>
+      <ContactWrapper>
+        <Text weight={"bold"}>Zarina Jewelry House</Text>
+        <Text>
+          <Image alt="" src={shop} /> ave. Yavornytskogo, 54
+        </Text>
+        <Text>
+          <Image alt="" src={phone} /> tel. +38 (063) 231 86 90
+        </Text>
+        <Text>
+          <Image alt="" src={clock} /> Mon-Sun: 10am - 7pm
+        </Text>
+      </ContactWrapper>
+    </Container>
+  );
+};
+
+export const Zaporizhzhia = () => {
+  return (
+    <Container>
+      <ContactWrapper>
+        <Text weight={"bold"}>Zarina Jewelry House</Text>
+        <Text>
+          <Image alt="" src={shop} /> ave. Soborniy, 83/85
+        </Text>
+        <Text>
+          <Image alt="" src={phone} /> tel. +38 (063) 979 86 30
+        </Text>
+        <Text>
+          <Image alt="" src={clock} /> Mon-Sun: 10am - 10pm
+        </Text>
+      </ContactWrapper>
+    </Container>
+  );
+};
+
+export const IvanoFrankivsk = () => {
+  return (
+    <Container>
+      <ContactWrapper>
+        <Text weight={"bold"}>Zarina Jewelry House</Text>
+        <Text>
+          <Image alt="" src={shop} /> st. Shashkevicha, 2
+        </Text>
+        <Text>
+          <Image alt="" src={phone} /> tel. +38 (034) 271 90 10
+        </Text>
+        <Text>
+          <Image alt="" src={clock} /> Mon-Sun: 10am - 7pm
+        </Text>
+      </ContactWrapper>
+    </Container>
+  );
+};
+
+export const Kremenchuh = () => {
+  return (
+    <Container>
+      <ContactWrapper>
+        <Text weight={"bold"}>Zarina Jewelry House</Text>
+        <Text>
+          <Image alt="" src={shop} /> st. Igora Serdyka, 37/18
+        </Text>
+        <Text>
+          <Image alt="" src={phone} /> tel. +38 (096) 298 80 84
+        </Text>
+        <Text>
+          <Image alt="" src={clock} /> Mon-Sun: 10am - 7pm
+        </Text>
+      </ContactWrapper>
+      <ContactWrapper>
+        <Text weight={"bold"}>Zarina Jewelry House</Text>
+        <Text>
+          <Image alt="" src={shop} /> st. Soborna, 10/7
+        </Text>
+        <Text>
+          <Image alt="" src={phone} /> tel. +38 (067) 530 81 26
+        </Text>
+        <Text>
+          <Image alt="" src={clock} /> Mon-Sun: 10am - 7pm
+        </Text>
+      </ContactWrapper>
+    </Container>
+  );
+};
+
+export const KryviyRih = () => {
+  return (
+    <Container>
+      <ContactWrapper>
+        <Text weight={"bold"}>Zarina Jewelry House</Text>
+        <Text>
+          <Image alt="" src={shop} /> st. Yesenina, 5
+        </Text>
+        <Text>
+          <Image alt="" src={phone} /> tel. +38 (067) 887 07 27
+        </Text>
+        <Text>
+          <Image alt="" src={clock} /> Mon-Sun: 10am - 7pm
+        </Text>
+      </ContactWrapper>
+    </Container>
+  );
+};
+
+export const Lytsk = () => {
+  return (
+    <Container>
+      <ContactWrapper>
+        <Text weight={"bold"}>Zarina Jewelry House</Text>
+        <Text>
+          <Image alt="" src={shop} /> ave. Voly, 17
+        </Text>
+        <Text>
+          <Image alt="" src={phone} /> tel. +38 (063) 979 89 33
+        </Text>
+        <Text>
+          <Image alt="" src={clock} /> Mon-Sun: 10am - 8pm
+        </Text>
+      </ContactWrapper>
+    </Container>
+  );
+};
+
+export const Lviv = () => {
+  return (
+    <Container>
+      <ContactWrapper>
+        <Text weight={"bold"}>Zarina Jewelry House</Text>
+        <Text>
+          <Image alt="" src={shop} /> sq. Mitskevicha, 1
+        </Text>
+        <Text>
+          <Image alt="" src={phone} /> tel. +38 (093) 148 52 57
+        </Text>
+        <Text>
+          <Image alt="" src={clock} /> Mon-Sun: 10am - 7pm
+        </Text>
+      </ContactWrapper>
+      <ContactWrapper>
+        <Text weight={"bold"}>Zarina Jewelry House</Text>
+        <Text>
+          <Image alt="" src={shop} /> st. Pid Dybom, 7-b
+        </Text>
+        <Text>
+          <Image alt="" src={phone} /> tel.+38 (063) 169 51 38
+        </Text>
+        <Text>
+          <Image alt="" src={clock} /> Mon-Sun: 10am - 10pm
+        </Text>
+      </ContactWrapper>
+    </Container>
+  );
+};
+
+export const Mykolaiv = () => {
+  return (
+    <Container>
+      <ContactWrapper>
+        <Text weight={"bold"}>Zarina Jewelry House</Text>
+        <Text>
+          <Image alt="" src={shop} /> ave. Central, 98
+        </Text>
+        <Text>
+          <Image alt="" src={phone} /> tel. +38 (051) 258 28 76
+        </Text>
+        <Text>
+          <Image alt="" src={clock} /> Mon-Sun: 10am - 9pm
+        </Text>
+      </ContactWrapper>
+    </Container>
+  );
+};
+
+export const Odessa = () => {
+  return (
+    <Container>
+      <ContactWrapper>
+        <Text weight={"bold"}>Zarina Jewelry House</Text>
+        <Text>
+          <Image alt="" src={shop} /> st. Rіshelievska, 23
+        </Text>
+        <Text>
+          <Image alt="" src={phone} /> tel. +38 (063) 979 95 98
+        </Text>
+        <Text>
+          <Image alt="" src={clock} /> Mon-Sun: 10am - 9pm
+        </Text>
+      </ContactWrapper>
+      <ContactWrapper>
+        <Text weight={"bold"}>Zarina Jewelry House</Text>
+        <Text>
+          <Image alt="" src={shop} /> st. Preobrazhanska, 34
+        </Text>
+        <Text>
+          <Image alt="" src={phone} /> tel. +38 (048) 233 80 34
+        </Text>
+        <Text>
+          <Image alt="" src={clock} /> Mon-Sun: 10am - 9pm
+        </Text>
+      </ContactWrapper>
+    </Container>
+  );
+};
+
+export const Rivne = () => {
+  return (
+    <Container>
+      <ContactWrapper>
+        <Text weight={"bold"}>Zarina Jewelry House</Text>
+        <Text>
+          <Image alt="" src={shop} /> st. Makarova, 23
+        </Text>
+        <Text>
+          <Image alt="" src={phone} /> tel. +38 (063) 979 89 15
+        </Text>
+        <Text>
+          <Image alt="" src={clock} /> Mon-Sun: 10am - 9pm
+        </Text>
+      </ContactWrapper>
+    </Container>
+  );
+};
+
+export const Kharkiv = () => {
+  return (
+    <Container>
+      <ContactWrapper>
+        <Text weight={"bold"}>Zarina Jewelry House</Text>
+        <Text>
+          <Image alt="" src={shop} /> st. Alchevskykh, 38
+        </Text>
+        <Text>
+          <Image alt="" src={phone} /> tel. +38 (057) 728 12 18
+        </Text>
+        <Text>
+          <Image alt="" src={clock} /> Mon-Sun: 10am - 7pm
+        </Text>
+      </ContactWrapper>
+    </Container>
+  );
+};
+
+export const Sumy = () => {
+  return (
+    <Container>
+      <ContactWrapper>
+        <Text weight={"bold"}>Zarina Jewelry House</Text>
+        <Text>
+          <Image alt="" src={shop} /> st. Mykhaila Lushpy, 4/1
+        </Text>
+        <Text>
+          <Image alt="" src={phone} /> tel. +38 (0542) 70 08 75
+        </Text>
+        <Text>
+          <Image alt="" src={clock} /> Mon-Sun: 10am - 8:40pm
+        </Text>
+      </ContactWrapper>
+    </Container>
+  );
+};
+
+export const Kherson = () => {
+  return (
+    <Container>
+      <ContactWrapper>
+        <Text weight={"bold"}>Zarina Jewelry House</Text>
+        <Text>
+          <Image alt="" src={shop} /> st. Zalaeherseh, 18
+        </Text>
+        <Text>
+          <Image alt="" src={phone} /> tel. +38 (055) 242 03 55
+        </Text>
+        <Text>
+          <Image alt="" src={clock} /> Mon-Sun: 10am - 9pm
+        </Text>
+      </ContactWrapper>
+    </Container>
+  );
+};
+
+export const Cherkasy = () => {
+  return (
+    <Container>
+      <ContactWrapper>
+        <Text weight={"bold"}>Zarina Jewelry House</Text>
+        <Text>
+          <Image alt="" src={shop} /> ave. Shevchenko, 197
+        </Text>
+        <Text>
+          <Image alt="" src={phone} /> tel. +38 (063) 979 98 86
+        </Text>
+        <Text>
+          <Image alt="" src={clock} /> Mon-St: 10am - 7pm
+        </Text>
+      </ContactWrapper>
+      <ContactWrapper>
+        <Text weight={"bold"}>Zarina Jewelry House</Text>
+        <Text>
+          <Image alt="" src={shop} /> st. Pryportova, 34
+        </Text>
+        <Text>
+          <Image alt="" src={phone} /> tel. +38 (063) 979 99 04
+        </Text>
+        <Text>
+          <Image alt="" src={clock} /> Mon-Sun: 10am - 8pm
+        </Text>
+      </ContactWrapper>
+    </Container>
+  );
+};
+
+export const Chernivtsi = () => {
+  return (
+    <Container>
+      <ContactWrapper>
+        <Text weight={"bold"}>Zarina Jewelry House</Text>
+        <Text>
+          <Image alt="" src={shop} /> st. Kobylianskoi, 22
+        </Text>
+        <Text>
+          <Image alt="" src={phone} /> tel. +38 (063) 979 92 19
+        </Text>
+        <Text>
+          <Image alt="" src={clock} /> Mon-Sun: 10am - 7pm
+        </Text>
+      </ContactWrapper>
+    </Container>
+  );
+};
+
+const Image = styled.img`
+  width: 15px;
+  height: 15px;
+  margin-right: 5px;
+  left: 0;
+`;
+const Container = styled.div`
+  width: 80%;
+  margin: 0 auto;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+const ContactWrapper = styled.div`
+  width: 250px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding-left: 3%;
+  margin: 15px;
+  border-left: 1px solid rgba(192, 192, 192, 0.5);
+  border-right: 1px solid rgba(192, 192, 192, 0.5);
+`;
+const Text = styled.p`
+  font-size: 14px;
+  line-height: 2;
+  margin-bottom: 3px;
+  ${props =>
+    props.weight === "bold" &&
+    css`
+      color: #5a5a5a;
+      margin: 0 auto;
+      font-weight: bold;
+    `}
+`;
