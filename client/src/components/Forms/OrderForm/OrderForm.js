2a23b92ed685f2a6434097dd94b197ef9f2c7990
Kseniia Zinchenko <ukrksenia2@gmail.com>
2020-04-18 22:51:48 +0300

    Added mobile-version

--- client/src/components/Forms/OrderForm/OrderForm.js
+++ client/src/components/Forms/OrderForm/OrderForm.js
@@ -1,11 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { Modal } from "../../Modal/Modal";
-import arrow from "./arrow.png";
 import { useHistory } from "react-router";
-import { mediaTablet } from "../../../styledComponents/MediaBreakpointsMixin";
 import { Wrapper } from "../../Checkout/checkout.styles";
-import styled from "styled-components";
-import Button from "@material-ui/core/Button";
+import { Button } from "../../common/Button/Button";
+import {
+  Content,
+  CreateAccountSubtitle,
+  CreateAccountTitle,
+  FormWrapper,
+  GoBackImage,
+  GoBackText,
+  GoBackWrapper,
+  Order
+} from "./orderForm.styles";
 
 export const OrderForm = props => {
   const { isModalOpen, onClose, email, icons } = props;
@@ -55,58 +62,3 @@ export const OrderForm = props => {
     </Modal>
   );
 };
-
-const FormWrapper = styled.form`
-  max-width: 1100px;
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  border: 1px solid #002d50;
-  background: #ffffff;
-`;
-
-const CreateAccountTitle = styled.div`
-  margin-top: 80px;
-  margin-bottom: 50px;
-  text-align: center;
-`;
-
-const Content = styled.div`
-  font-size: 24px;
-  line-height: 40px;
-  text-transform: uppercase;
-  ${mediaTablet(`
-font-size: 16px;
-  `)}
-`;
-
-const CreateAccountSubtitle = styled.div`
-  margin-bottom: 80px;
-  font-size: 14px;
-`;
-
-const GoBackWrapper = styled.div`
-  display: flex;
-  margin-bottom: 80px;
-`;
-const GoBackText = styled.div`
-  font-size: 14px;
-`;
-
-export const GoBackImage = styled.div`
-  width: 15px;
-  height: 15px;
-  background-image: url(${arrow});
-  background-repeat: no-repeat;
-  background-size: contain;
-  color: #80858d;
-`;
-const Order = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  margin: 10px auto;
-  width: 98%;
-`;
